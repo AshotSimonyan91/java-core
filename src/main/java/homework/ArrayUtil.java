322675db34a7bd2f2349496d1698f7a0b3308d96
Ashot Simonyan <ashotsimonyan1991@mail.ru>
2022-12-21 01:03:06 +0400

    add FigurePainter2,ArrayUtil

---
+++ src/main/java/homework/ArrayUtil.java
@@ -0,0 +1,27 @@
+package homework;
+
+public class ArrayUtil {
+
+    public static void main(String[] args) {
+        int[] array = {-2, 5, 8, 23, 54, 754, 2466, 3, 97, 45};
+        int maxValue = 0;
+        int minValue = 0;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (maxValue < array[i]) {
+                maxValue = array[i];
+            }
+        }
+        System.out.println("max value: " + maxValue);
+
+        for (int i = 0; i < array.length; i++) {
+            if (minValue > array[i]) {
+                minValue = array[i];
+            }
+        }
+        System.out.println("min value: " + minValue);
+    }
+}
---
+++ src/main/java/homework/homework2/FigurePainter2.java
@@ -0,0 +1,38 @@
+package homework.homework2;
+
+public class FigurePainter2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 3 - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
