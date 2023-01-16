b2721f1cb0ee0bbe4a8ad2ce3f02c1dd13f7bd71
Ashot Simonyan <ashotsimonyan1991@mail.ru>
2023-01-16 19:37:46 +0400

    add other classes of chapter6

--- src/main/java/chapter6/Box.java
+++ src/main/java/chapter6/Box.java
@@ -6,8 +6,13 @@ public class Box {
     double height;
     double depth;
 
-    void volume() {
-        System.out.print("Объем равен ");
-        System.out.println(width * height * depth);
+    Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    double volume() {
+        return width * height * depth;
     }
 }
